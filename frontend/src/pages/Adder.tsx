7f9f0be02c4127187f8969e9bab397d69a330a38
송찬흠 <bsk9212@gmail.com>
2021-10-15 17:20:29 +0900

    form 수정

--- frontend/src/pages/Adder.tsx
+++ frontend/src/pages/Adder.tsx
@@ -8,37 +8,52 @@ import {
     Button,
     Box,
     Flex,
+    useColorModeValue
   } from "@chakra-ui/react"
 
 function Adder(props:any) {
  
   return (
-    <>
-    <Flex>
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg">
-      <FormControl id="first-name" isRequired>            
-            <FormLabel>First name</FormLabel>
-            <Input placeholder="First name" />
-            <FormHelperText>Select only if you're a fan.</FormHelperText>
+    <Box
+    bg={useColorModeValue('gray.50', 'inherit')}
+    minH="100vh"
+    py="12"
+    px={{ base: '4', lg: '8' }}
+  >
+    <Box     
+    bg={useColorModeValue('white', 'gray.700')}
+    py="8"
+    px={{ base: '4', md: '10' }}
+    shadow="base"
+    rounded={{ sm: 'lg' }}>
+    <Flex w="100%" p={2} align="center" direction="column">
+      <Flex direction="row">
+      <Box p='4'w="320">
+        <FormControl id="first-name" isRequired>            
+          <FormLabel>First name</FormLabel>
+          <Input placeholder="First name" />
         </FormControl>
       </Box>
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg">
-      <FormControl id="first-name" isRequired>            
-            <FormLabel>First name</FormLabel>
-            <Input placeholder="First name" />
-            <FormHelperText>Select only if you're a fan.</FormHelperText>
+      <Box p='4'>
+        <FormControl id="first-name" isRequired>            
+          <FormLabel>First name</FormLabel>
+          <Input placeholder="First name" />
         </FormControl>
       </Box>
-    </Flex>
-       
-        <Button
+      </Flex>
+      <Button
             mt={4}
             colorScheme="teal"
             isLoading={props.isSubmitting}
-            type="submit">
+            type="submit"
+            alignSelf="center">
             저장
         </Button>
-    </>
+    </Flex>
+       
+       
+    </Box>
+    </Box>
   )
 }
 
